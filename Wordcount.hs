b50cb034b37b493ed0a70ebaed1f6617c1a1bb7e
Jeroen Janssen <jejansse+github@gmail.com>
2013-02-04 23:26:26 +0100

    Altered configuration and main function to incorporate changes to haskoop library.

--- Wordcount.hs
+++ Wordcount.hs
@@ -5,8 +5,10 @@ import Haskoop
 
 configuration :: Configuration
 configuration = defaultConfiguration {
-	input = Just ["access.log"],
-	output = Just "ipcounts"
+	input = Just ["file:///Users/jeroenthuis/Workspace/Haskoop2/access.log"],
+	output = Just "/user/jeroenthuis/haskoop/ipcounts",
+	hadoop = Just "/Users/jeroenthuis/Workspace/hadoop-1.0.3/bin/hadoop",
+	streamingJar = Just "/Users/jeroenthuis/Workspace/hadoop-1.0.3/contrib/streaming/hadoop-streaming-1.0.3.jar"
 }
 
 mapper1 :: Int -> String -> IO [(String, Int)]
@@ -15,6 +17,8 @@ mapper1 _ value = return [(w,1) | w <- words value]
 reducer1 :: String -> [Int] -> IO [(String, Int)]
 reducer1 key values = return [(key, sum values)]
 
+wcJob :: Job Int String String Int
+wcJob = job mapper1 reducer1
+
 main :: IO ()
-main = runMapper mapper1
---main = runReducer reducer1
+main = runHaskoop configuration wcJob
