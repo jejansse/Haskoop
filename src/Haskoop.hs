c646bad55a266a36ea30b33e423fbbbdfd4d17b6
Jeroen Janssen <jejansse+github@gmail.com>
2013-02-06 21:49:51 +0100

    Removed the monstrous type constraint using a type class.

--- src/Haskoop.hs
+++ src/Haskoop.hs
@@ -13,24 +13,34 @@ type Mapper k1 v1 k2 v2 = k1 -> v1 -> IO [(k2,v2)]
 
 type Reducer k1 v1 k2 v2 = k1 -> [v1] -> IO [(k2,v2)]
 
-data Iteration k1 v1 k3 v3 = forall k2 v2 . (Eq k2, Eq v2, Parsable k2, Parsable v2, Showable k2, Showable v2) => Iteration (Mapper k1 v1 k2 v2) (Reducer k2 v2 k3 v3)
+data Iteration k1 v1 k3 v3 = forall k2 v2 . (KeyValue k2, KeyValue v2) => Iteration (Mapper k1 v1 k2 v2) (Reducer k2 v2 k3 v3)
 
-data Job k1 v1 k3 v3 = EmptyJob | forall k2 v2 . (Eq k2, Eq v2, Parsable k2, Parsable v2, Showable k2, Showable v2) => ConsJob (Iteration k1 v1 k2 v2) (Job k2 v2 k3 v3)
+data Job k1 v1 k3 v3 = EmptyJob | forall k2 v2 . (KeyValue k2, KeyValue v2) => ConsJob (Iteration k1 v1 k2 v2) (Job k2 v2 k3 v3)
+
+class (Show a, Read a, Eq a) => KeyValue a where
+	readKeyValue :: String -> a
+	readKeyValue = read
+	showKeyValue :: a -> String
+	showKeyValue = show
+instance KeyValue String where
+	readKeyValue s = s
+	showKeyValue s = s
+instance KeyValue Int where
+	readKeyValue = read
+	showKeyValue = show
 
 
--- FIXME: can we get rid of this monstrous type constraint?
-job :: (Parsable k1, Parsable v1, Parsable k2, Parsable v2, Eq k2, Eq v2, Parsable k3, Parsable v3, Showable k1,
- Showable v1, Showable k2, Showable v2, Showable k3, Showable v3, Eq k3, Eq v3)
+job :: (KeyValue k1, KeyValue v1, KeyValue k2, KeyValue v2, KeyValue k3, KeyValue v3)
   => Mapper k1 v1 k2 v2 -> Reducer k2 v2 k3 v3 -> Job k1 v1 k3 v3
 job m r = ConsJob (Iteration m r) EmptyJob
 
 
-(>>>) :: (Parsable k1, Parsable v1, Showable k2, Showable v2, Parsable k2, Parsable v2, Eq k2, Eq v2, Showable k3, Showable v3) 
+(>>>) :: (KeyValue k1, KeyValue v1, KeyValue k2, KeyValue v2, KeyValue k3, KeyValue v3)
       => Iteration k1 v1 k2 v2 -> Job k2 v2 k3 v3 -> Job k1 v1 k3 v3
 (>>>) = ConsJob
 
 
-runHaskoop :: (Showable k1, Showable v1, Showable k2, Showable v2, Parsable k1, Parsable v1, Parsable k2, Parsable v2) 
+runHaskoop :: (KeyValue k1, KeyValue v1, KeyValue k2, KeyValue v2)
            => Configuration -> Job k1 v1 k2 v2 -> IO ()
 runHaskoop conf j = do
 	args <- getArgs
@@ -40,7 +50,7 @@ runHaskoop conf j = do
 		else parseAndRunJobIteration j args
 
 
-parseAndRunJobIteration :: (Showable k1, Showable v1, Showable k2, Showable v2, Parsable k1, Parsable v1, Parsable k2, Parsable v2) 
+parseAndRunJobIteration :: (KeyValue k1, KeyValue v1, KeyValue k2, KeyValue v2) 
 						=> Job k1 v1 k2 v2 -> [String] -> IO ()
 parseAndRunJobIteration j ("map":[i]) = runJobIteration j (read i) IterationMapper
 parseAndRunJobIteration j ("reduce":[i]) = runJobIteration j (read i) IterationReducer
@@ -72,7 +82,7 @@ type IterationNumber = Int
 data IterationPhase = IterationMapper | IterationReducer
 
 
-runJobIteration :: (Parsable k1, Parsable v1, Parsable k2, Parsable v2, Showable k1, Showable v1, Showable k2, Showable v2) 
+runJobIteration :: (KeyValue k1, KeyValue v1, KeyValue k2, KeyValue v2) 
 				=> Job k1 v1 k2 v2 -> IterationNumber -> IterationPhase -> IO ()
 runJobIteration EmptyJob _ _ = return ()
 runJobIteration (ConsJob (Iteration m _) _) 0 IterationMapper = runMapper m
@@ -99,43 +109,24 @@ defaultConfiguration = Configuration {
 	streamingJar = Nothing
 }
 
-class Parsable a where
-	parse :: String -> a
-
-instance Parsable Int where
-	parse = read -- FIXME: can we avoid the unpack and read??
-
-instance Parsable String where
-	parse s = s
-
-class Showable a where
-	showit :: a -> String
-
-instance Showable String where
-	showit s = s
-
-instance Showable Int where
-	showit = show
-
-
-runMapper :: (Parsable k1, Parsable v1, Showable v2, Showable k2) 
+runMapper :: (KeyValue k1, KeyValue v1, KeyValue k2, KeyValue v2)
 		  => Mapper k1 v1 k2 v2 -> IO ()
 runMapper mapper = do
 	contents <- getContents
 	mapOutput <- mapM (uncurry mapper . parseKeyValue) $ lines contents
-	mapM_ putStrLn $ map showKeyValue $ concat mapOutput
+	mapM_ putStrLn $ map printKeyValue $ concat mapOutput
 
 
-runReducer :: (Parsable k1, Parsable v1, Eq k1, Eq v1, Showable v2, Showable k2) 
+runReducer :: (KeyValue k1, KeyValue v1, KeyValue k2, KeyValue v2)
            => Reducer k1 v1 k2 v2 -> IO ()
 runReducer reducer = do
 	contents <- getContents
 	reduceOutput <- mapM (uncurry reducer) $ groupMapOutput $ map parseKeyValue $ lines contents
-	mapM_ putStrLn $ map showKeyValue $ concat reduceOutput
+	mapM_ putStrLn $ map printKeyValue $ concat reduceOutput
 
 
-parseKeyValue :: (Parsable k, Parsable v) => String -> (k,v)
-parseKeyValue = (parse *** parse) . split (== '\t')
+parseKeyValue :: (KeyValue k, KeyValue v) => String -> (k,v)
+parseKeyValue = (readKeyValue *** readKeyValue) . split (== '\t')
 
 
 split :: (a -> Bool) -> [a] -> ([a], [a])
@@ -143,8 +134,8 @@ split p xs = let (group, rest) = break p xs
 			 in (group, tail rest)
 
 
-showKeyValue :: (Showable k, Showable v) => (k,v) -> String
-showKeyValue (k,v) = showit k ++ "\t" ++ showit v
+printKeyValue :: (KeyValue k, KeyValue v) => (k,v) -> String
+printKeyValue (k,v) = showKeyValue k ++ "\t" ++ showKeyValue v
 
 
 groupMapOutput :: (Eq k, Eq v) => [(k,v)] -> [(k,[v])]
